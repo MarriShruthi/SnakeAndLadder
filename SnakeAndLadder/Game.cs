050e29470c63b67ef17a9fc979ffec9fc1d1593d
Shruthi <shruthimarri97@gmail.com>
2021-03-19 11:05:06 +0530

    [Shruthi] Add : single player at start position

---
+++ SnakeAndLadder/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class Game
+    {
+        const int START_POSITION = 0;
+        public void Play()
+        {
+            Console.WriteLine(" Single player at start position = "+START_POSITION);
+        }
+    }
+}
+
+
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,7 +6,9 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder game ");
+            Console.WriteLine("Welcome to Snake and Ladder game");
+            Game game = new Game();
+            game.Play();
         }
     }
 }
